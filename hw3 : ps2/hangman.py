fa8497094396fdbd409beaf1ceb9673ec3942b83
akurbanov <kurbanovahmad@gmail.com>
2019-09-13 16:54:47 -0400

    added comments into hangman

--- hw3 : ps2/hangman.py
+++ hw3 : ps2/hangman.py
@@ -4,14 +4,15 @@
 # Time spent: 8 hours
 
 # Hangman Game
+#
 # Hangman implements a user-compuer interaction game. I spent 
 # sufficient time testing each function and passed all my own 
 # test cases and ones which were collaboratively developed by 
 # the whole class. I created total of 12 test-cases for different 
 # parts of this assignment; they all can be found in test_hangman.py
 # I also developed my own version of the hangman, which is called
-# my_hangman and can be found below. To play my version of hangman
-# you have to two lines of the code in the main method. 
+# my_hangman and can be found below. To play my version of the hangman
+# you have to uncomment the last two lines of the code below (line 563). 
 # -----------------------------------
 
 import random
@@ -67,7 +68,7 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-
+    # if each letter in secret word is not guessed yet, return false
     for letter in secret_word:
       if letter not in letters_guessed:
         return False
@@ -84,8 +85,8 @@ def get_guessed_word(secret_word, letters_guessed):
     '''
     guessed_word = []     
 
-    # check each letter if it is in letters_guessed, 
-    # if it is append it, otherwise append "_ "
+    # check each letter, if it is in letters_guessed, 
+    # if it is append it to the list, otherwise append "_ "
     for letter in secret_word:
       if letter in letters_guessed:
         guessed_word.append(letter)
@@ -105,7 +106,7 @@ def get_available_letters(letters_guessed):
     '''
     letters_not_guessed = string.ascii_lowercase
 
-    # removing each letter from letters guessed from ascii string
+    # removing each guessed letter from ascii string
     for letter in letters_guessed:
       if letter in letters_not_guessed:
         letters_not_guessed = letters_not_guessed.replace(letter, '')
@@ -115,7 +116,7 @@ def get_available_letters(letters_guessed):
     
     
 def hangman(secret_word):
-    len_word = len(secret_word)
+    len_word = len(secret_word) 
     guesses_remaining = 6
     warnings_remaining = 3
     letters_guessed = []
@@ -127,6 +128,7 @@ def hangman(secret_word):
     print("You have 3 warnings left.")
     print("----------------------------------------------------------")
 
+    # play the game until we have guesses left
     while guesses_remaining > 0:
       print("You have " + str(guesses_remaining) + " guesses left!")
       print("Available letters: " + get_available_letters(letters_guessed))
@@ -155,7 +157,7 @@ def hangman(secret_word):
             print("You have no warnings left so you lose one guess: " + get_guessed_word(secret_word, letters_guessed))
             guesses_remaining -= 1
 
-      # checking if it is single letter 
+      # checking if input is a single letter 
       elif len(letter_from_user) != 1: 
           warnings_remaining -= 1
           if warnings_remaining >= 0:
@@ -183,6 +185,7 @@ def hangman(secret_word):
       print("----------------------------------------------------------")
       if is_word_guessed(secret_word, letters_guessed): break
 
+    # calculating total score after the game is finished and the word is guessed
     if is_word_guessed(secret_word, letters_guessed): 
       num_of_letters = 0
       for letter in letters_guessed:
@@ -244,6 +247,8 @@ def show_possible_matches(my_word):
     '''
     matches = []
     matched = False
+
+    # append each word from the word list which matches my_word
     for word in wordlist:
       if match_with_gaps(my_word, word):
         matches.append(word)
@@ -271,17 +276,19 @@ def hangman_with_hints(secret_word):
     print("You have 3 warnings left.")
     print("----------------------------------------------------------")
 
+    # play the game until we have guesses left
     while guesses_remaining > 0:
       print("You have " + str(guesses_remaining) + " guesses left!")
       print("Available letters: " + get_available_letters(letters_guessed))
       
       letter_from_user = input("Please guess a letter: ").lower()
 
+      # if input is asterisk, show hints
       if letter_from_user == '*':
         show_possible_matches(get_guessed_word(secret_word, letters_guessed))
-      
+    
       # checking if letter is in alphabet
-      if not letter_from_user.isalpha() and letter_from_user != '*':
+      elif not letter_from_user.isalpha():
           warnings_remaining -= 1
           if warnings_remaining >= 0:
             print("Oops! That is not a valid letter. ")
@@ -302,7 +309,7 @@ def hangman_with_hints(secret_word):
             print("You have no warnings left so you lose one guess: " + get_guessed_word(secret_word, letters_guessed))
             guesses_remaining -= 1
 
-      # checking if it is single letter 
+      # checking if input is a single letter 
       elif len(letter_from_user) != 1: 
           warnings_remaining -= 1
           if warnings_remaining >= 0:
@@ -313,8 +320,8 @@ def hangman_with_hints(secret_word):
             print("You have no warnings left so you lose one guess: " + get_guessed_word(secret_word, letters_guessed))
             guesses_remaining -= 1
 
-      # else letter is valid and was not guessed and not asterisk
-      elif letter_from_user != '*':
+      # at this point letter is a valid letter
+      else:
           letters_guessed.append(letter_from_user)  
           
           if letter_from_user in secret_word:
@@ -330,7 +337,7 @@ def hangman_with_hints(secret_word):
       print("----------------------------------------------------------")
       if is_word_guessed(secret_word, letters_guessed): break
 
-
+     # calculating total score after the game is finished and the word is guessed
     if is_word_guessed(secret_word, letters_guessed): 
       num_of_letters = 0
       for letter in letters_guessed:
@@ -458,17 +465,19 @@ def my_hangman(secret_word):
     show_hangman_pic((guesses_remaining+1)*(-1))
     print("----------------------------------------------------------")
 
+    # play the game until we have to guesses left
     while guesses_remaining > 0:
       print("You have " + str(guesses_remaining) + " guesses left!")
       print("Available letters: " + get_available_letters(letters_guessed))
       
       letter_from_user = input("Please guess a letter: ").lower()
 
+      # checking if input is asterisk to get hints
       if letter_from_user == '*':
         show_possible_matches(get_guessed_word(secret_word, letters_guessed))
       
       # checking if letter is in alphabet
-      if not letter_from_user.isalpha() and letter_from_user != '*':
+      elif not letter_from_user.isalpha():
           warnings_remaining -= 1
           if warnings_remaining >= 0:
             print("Oops! That is not a valid letter. ")
@@ -489,7 +498,7 @@ def my_hangman(secret_word):
             print("You have no warnings left so you lose one guess: " + get_guessed_word(secret_word, letters_guessed))
             guesses_remaining -= 1
 
-      # checking if it is single letter 
+      # checking if the input is a single letter 
       elif len(letter_from_user) != 1: 
           warnings_remaining -= 1
           if warnings_remaining >= 0:
@@ -500,8 +509,8 @@ def my_hangman(secret_word):
             print("You have no warnings left so you lose one guess: " + get_guessed_word(secret_word, letters_guessed))
             guesses_remaining -= 1
 
-      # else letter is valid and was not guessed and not asterisk
-      elif letter_from_user != '*':
+      # at this point input is a valid letter
+      else:
           letters_guessed.append(letter_from_user)  
           
           if letter_from_user in secret_word:
@@ -514,14 +523,15 @@ def my_hangman(secret_word):
             
             print("Oops! That letter is not in my word: " + get_guessed_word(secret_word, letters_guessed))
       
-
+      # if amount of guesses left changed, print out the picture
       if old_gusses_reamaining > guesses_remaining:
+        # getting the right index of the hangman in the pack of pictures
         show_hangman_pic((guesses_remaining+1)*(-1))
         old_gusses_reamaining = guesses_remaining
       print("----------------------------------------------------------")
       if is_word_guessed(secret_word, letters_guessed): break
 
-
+    #  # calculating total score after the game is finished and the word is guessed
     if is_word_guessed(secret_word, letters_guessed): 
       num_of_letters = 0
       for letter in letters_guessed:
@@ -532,20 +542,23 @@ def my_hangman(secret_word):
       print("Congratulations, you won!")
       print("Your total score for this game is: " + str(total_score))
     else: 
-      print("Sorry, you ran out of guesses. The word was " + secret_word + ".")
+      print("Sorry, you ran out of guesses. The word was " + secret_word + ".") 
 
 
 
 if __name__ == "__main__":
-    # secret_word = choose_word(wordlist)
-    # hangman(secret_word)
+  # secret_word = choose_word(wordlist)
+  # hangman(secret_word)
 
 ###############
 
-    secret_word = choose_word(wordlist)
-    hangman_with_hints(secret_word)
+  secret_word = choose_word(wordlist)
+  hangman_with_hints(secret_word)
 
 ###############
 
-    # secret_word = choose_word(wordlist)
-    # my_hangman(secret_word)
+  # I added a modification into the game, commentout two lines 
+  # above and uncomment two lines below to check it out.   
+
+  # secret_word = choose_word(wordlist)
+  # my_hangman(secret_word)
