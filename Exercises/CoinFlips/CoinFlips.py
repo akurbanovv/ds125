0d3168e81934a3b21141d6788f3cb32322dfa1b2
akurbanov <kurbanovahmad@gmail.com>
2019-09-26 15:11:33 -0400

    coinflips done

--- Exercises/CoinFlips/CoinFlips.py
+++ Exercises/CoinFlips/CoinFlips.py
@@ -1,40 +1,37 @@
 #
 # Assigment: CoinFlips
 # Name: Akhmadjon Kurbanov
-#
 
 import random
 
-
 def flipCoin():
-    #1 is heads, 0 is tails
     return random.choice(['T','H'])
 
-def gotMatch(n, targetMatch):
+def gotMatch(n):
     result = ''
+    counter = 0
     for i in range(n):
-        result += flipCoin()
-    if targetMatch in result: return True
-    else: return False
+        if flipCoin() == 'H':
+            counter += 1 
+            if counter >= 2: return True
+    return False
 
-def matchProb(n, numTrials):
+def empirProb(n, numTrials):
     numHits = 0
-    targetMatch = 'HH'
     for i in range(numTrials):
-        if gotMatch(n, targetMatch):
+        if gotMatch(n):
             numHits += 1
     return numHits/numTrials
-    
+
+def theorProb(n):
+    return 1 - ((1+ n)/(2**n))
+
 if __name__ == '__main__':
-    
+    numTrials = 10000
+    numFlips = [2, 3, 4, 5, 10, 20, 100]
 
-
-
-
-
-        
-
-
-
-
-
+    for n in numFlips:
+        print('For N =', n)
+        print('The Theoretical Probability:', round(theorProb(n), 4))
+        print('The Empirical Probability:  ', (empirProb(n, numTrials)))
+        print()
